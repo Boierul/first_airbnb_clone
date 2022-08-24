af192f65d22c19c7d76bc847542c5195736ba764
Boierul <49337867+Boierul@users.noreply.github.com>
2022-08-25 00:46:56 +0200

    Added Footer and experimented with Bootstrap Footer.

--- src/App.js
+++ src/App.js
@@ -2,20 +2,25 @@ import './App.css';
 import Navbar from "./components/Navbar/Navbar";
 import Features from "./components/Features/Features";
 import Cards from "./components/Cards/Cards";
+import Footer from "./components/Footer/Footer";
 import cardData from "./components/RawData/Data";
+import { MDBContainer} from 'mdb-react-ui-kit';
 
 
 function App() {
     const cardElements = cardData.map((card) => {
-        return <Cards key={card.id} card={card} />;
+        return <Cards key={card.id} card={card}/>;
     });
 
     return (
+        <MDBContainer fluid>
         <div>
             <Navbar/>
             <Features/>
-            <section className="cardlist">{cardElements}</section>
+            <section className="card-list">{cardElements}</section>
+            <Footer/>
         </div>
+        </MDBContainer>
     );
 }
 
