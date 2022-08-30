d44b455baeb0d72f342a121282e4db6c4b61b4a7
Boierul <49337867+Boierul@users.noreply.github.com>
2022-08-30 10:14:36 +0200

    Minor changes. Avoid Bootstrap as it makes things very narrow in scope.

--- src/App.js
+++ src/App.js
@@ -7,14 +7,16 @@ import cardData from "./components/RawData/Data";
 
 function App() {
     const cardElements = cardData.map((card) => {
-        return <Cards key={card.id} card={card}/>;
+        return <Cards key={card.id}  card={card}/>;
     });
 
     return (
         <div>
             <Navbar/>
             <Features/>
-            <section className="card-list">{cardElements}</section>
+            <section className="card-list">
+                {cardElements}
+            </section>
             <Footer/>
         </div>
     );
